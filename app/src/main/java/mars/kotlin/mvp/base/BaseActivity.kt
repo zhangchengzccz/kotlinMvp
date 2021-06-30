882ae1528629c04f4cd50fd7cc7765cc9df80f2f
Mars <zhangchengzccz@126.com>
2021-06-30 22:20:27 +0800

    新建基类和配置

---
+++ app/src/main/java/mars/kotlin/mvp/base/BaseActivity.kt
@@ -0,0 +1,19 @@
+package mars.kotlin.mvp.base
+
+import android.support.v7.app.AppCompatActivity
+
+/**
+ * 创建日期：2021/6/30 0030 22:17
+ * @author Mars
+ * @version 1.0
+ * 包名： mars.kotlin.mvp.base
+ * 类说明：
+ */
+abstract class BaseActivity<P> : AppCompatActivity() where  P : IBasePresenter {
+
+
+
+
+
+
+}
---
+++ app/src/main/java/mars/kotlin/mvp/base/IBasePresenter.kt
@@ -0,0 +1,17 @@
+package mars.kotlin.mvp.base
+
+/**
+ * 创建日期：2021/6/30 0030 21:52
+ * @author Mars
+ * @version 1.0
+ * 包名： mars.kotlin.mvp.base
+ * 类说明：
+ */
+interface IBasePresenter {
+
+    /**
+     * 销毁离开
+     */
+    fun unAttachView()
+
+}
